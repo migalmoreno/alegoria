5d5357f50976ecd9868b52938ab15556a9bd4f4f
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-12-21 23:10:07 +0100

    fix: remove image loading indicator and fix video config

--- src/frontend/src/components/posts/Image.tsx
+++ src/frontend/src/components/posts/Image.tsx
@@ -6,7 +6,6 @@ import { UserAvatar } from "../UserAvatar";
 import ShakaVideo from "shaka-video-element/react";
 import { useEffect, useRef } from "react";
 import CustomVideoElement from "shaka-video-element";
-import { LoadingContainer } from "../LoadingContainer";
 import { ErrorContainer } from "../ErrorContainer";
 
 export interface ImagePostProps<T> {
@@ -19,7 +18,7 @@ export const ImagePost = <T,>({
   videoExtractor,
 }: ImagePostProps<T>) => {
   const { url } = useParams();
-  const { data, error, isPending, isError } = useQuery({
+  const { data, error, isFetched, isPending, isError } = useQuery({
     queryKey: [`/posts/${url}`],
     select: (res: T) => extractor(res),
   });
@@ -28,9 +27,7 @@ export const ImagePost = <T,>({
 
   const { data: video } = useQuery<PostResponse<YtdlItem>>({
     enabled: !!data?.videoUrl,
-    queryKey: [
-      `/posts/${data?.videoUrl && encodeURIComponent(data?.videoUrl)}`,
-    ],
+    queryKey: [`/posts/${encodeURIComponent(String(data?.videoUrl))}`],
   });
 
   useEffect(() => {
@@ -42,7 +39,7 @@ export const ImagePost = <T,>({
       if (player) {
         player.configure({
           preferredVideoCodecs: ["av01", "vp9", "avc1"],
-          preferreddAudioCodecs: ["opus", "mp4a"],
+          preferredAudioCodecs: ["opus", "mp4a"],
           manifest: { disableVideo: false },
           streaming: {
             retryParameters: {
@@ -52,74 +49,71 @@ export const ImagePost = <T,>({
             },
           },
         });
-
-        videoRef.current.src = videoData.url;
       }
+      videoRef.current.src = videoData.url;
     }
   }, [video]);
-
-  if (isPending) {
-    return <LoadingContainer />;
-  }
 
   if (isError) {
     return <ErrorContainer error={error} />;
   }
 
-  return (
-    <div className="flex flex-col items-center justify-center flex-auto bg-black text-white box-border lg:p-8">
-      <div className="w-full lg:h-[550px] lg:w-4/5 lg:border border-neutral-600 flex-auto">
-        <div className="h-[calc(100dvh-60px)] lg:h-full flex flex-auto flex-col md:flex-row">
-          <div className="h-0 md:h-full w-full flex-auto">
-            {data.type === "video" ? (
-              <ShakaVideo
-                className="min-w-full max-w-full min-h-full max-h-full object-cover h-dvh"
-                ref={videoRef}
-                poster={`${data.url}`}
-                controls
-              />
-            ) : (
-              <img
-                src={`${data.url}`}
-                className="object-cover min-w-full max-w-full min-h-full max-h-full"
-              />
-            )}
-          </div>
-          <div className="md:border-l border-neutral-600 lg:w-[300px] shrink-0 lg:p-0 flex flex-col gap-y-2 py-4 p-2">
-            <div className="md:border-b border-neutral-600 lg:p-4 flex gap-x-2 items-center px-2">
-              {data.authorThumbnail && (
-                <UserAvatar
-                  thumbnail={data.authorThumbnail}
-                  extraClassNames="h-10 w-10"
+  if (isFetched && !isPending) {
+    return (
+      <div className="flex flex-col items-center justify-center flex-auto bg-black text-white box-border lg:p-8">
+        <div className="w-full lg:h-[550px] lg:w-4/5 lg:border border-neutral-800 flex-auto">
+          <div className="h-[calc(100dvh-60px)] lg:h-full flex flex-auto flex-col md:flex-row">
+            <div className="h-0 md:h-full w-full flex-auto">
+              {data.type === "video" ? (
+                <ShakaVideo
+                  className="min-w-full max-w-full min-h-full max-h-full object-cover h-dvh"
+                  ref={videoRef}
+                  poster={`${data.url}`}
+                  controls
+                />
+              ) : (
+                <img
+                  src={`${data.url}`}
+                  className="object-cover min-w-full max-w-full min-h-full max-h-full"
                 />
               )}
-              {data.authorUrl ? (
-                <Link
-                  href={`/post/${encodeURIComponent(data.authorUrl)}`}
-                  className="font-semibold text-sm"
-                >
-                  {data.user}
-                </Link>
-              ) : (
-                <span className="font-semibold text-sm">{data.user}</span>
-              )}
-              {data.date && (
-                <span
-                  className="text-neutral-400 text-sm shrink-0"
-                  title={data.date.toLocaleString()}
-                >
-                  {formatTimeAgo(data.date)}
-                </span>
+            </div>
+            <div className="md:border-l border-neutral-800 lg:w-[300px] shrink-0 md:p-0 flex flex-col gap-y-2 py-4 p-2">
+              <div className="md:border-b border-neutral-800 md:p-4 flex gap-x-2 items-center px-2">
+                {data.authorThumbnail && (
+                  <UserAvatar
+                    thumbnail={data.authorThumbnail}
+                    extraClassNames="h-10 w-10"
+                  />
+                )}
+                {data.authorUrl ? (
+                  <Link
+                    href={`/post/${encodeURIComponent(data.authorUrl)}`}
+                    className="font-semibold text-sm"
+                  >
+                    {data.user}
+                  </Link>
+                ) : (
+                  <span className="font-semibold text-sm">{data.user}</span>
+                )}
+                {data.date && (
+                  <span
+                    className="text-neutral-400 text-sm shrink-0"
+                    title={data.date.toLocaleString()}
+                  >
+                    {formatTimeAgo(data.date)}
+                  </span>
+                )}
+              </div>
+              {data.description && (
+                <div className="px-2 lg:px-4 text-sm/6 text-neutral-200">
+                  {data.description}
+                </div>
               )}
             </div>
-            {data.description && (
-              <div className="px-2 lg:px-4 text-sm/6 text-neutral-200">
-                {data.description}
-              </div>
-            )}
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
